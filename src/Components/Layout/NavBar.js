fc76b5d0df0527c87208dab54ff2206233a43720
vytcka4 <vytautas.freelance@gmail.com>
2022-07-05 22:33:17 +0300

    commit

--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -1,8 +1,37 @@
-import React from "react";
-import classes from ".Navbar.module.css";
+import React, { useRef } from "react";
+import classes from "./Navbar.module.css";
+import { FaTimes, FaBars } from "react-icons/fa";
 
-function Navbar() {
-  return <nav></nav>;
-}
+const Navbar = React.forwardRef(function Navbar(props, ref) {
+  const navRef = useRef();
+  const responsiveNav = () => {
+    navRef.current.classList.toggle(`${classes.responsiveBtn}`);
+  };
+
+  return (
+    <header>
+      <nav ref={ref} className={classes["nav"]}>
+        <a className={classes.linkNav} href="#contact-container">
+          Contact info
+        </a>
+        <a className={classes.linkNav} href="#about-tittle">
+          About me
+        </a>
+        <a className={classes.linkNav} href="#projects-tittle">
+          Projects
+        </a>
+        <a className={classes.linkNav} href="main">
+          Home
+        </a>
+        <button className={classes.responsiveBtn}>
+          <FaTimes onClick={responsiveNav} />
+        </button>
+      </nav>
+      <button className={classes.responsiveBtn}>
+        <FaBars onClick={responsiveNav} />
+      </button>
+    </header>
+  );
+});
 
 export default Navbar;
