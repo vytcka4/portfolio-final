af6ba01638b23e77ee67cc8194aba8318daeba70
vytcka4 <vytautas.freelance@gmail.com>
2022-07-11 21:37:08 +0300

    commit

--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -1,21 +1,44 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useMemo } from "react";
 import classes from "./Navbar.module.css";
 import { FaTimes, FaBars } from "react-icons/fa";
 
 const Navbar = function Navbar(props) {
-  const navRef = useRef();
-  const responsiveNav = () => {
-    navRef.current.classList.toggle(`${classes.responsiveBtn}`);
+  const [responsiveNavState, setResponsiveNavState] = useState();
+  const responsiveHandler = () => {
+    setResponsiveNavState((state) => !state);
   };
 
+  let responsive;
+  let hidden;
+  let responsiveHeader;
+
+  if (responsiveNavState === true) {
+    responsive = classes.responsive;
+    hidden = classes.hidden;
+    responsiveHeader = classes.responsiveHeader;
+  } else {
+    responsive = "";
+    hidden = "";
+    responsiveHeader = "";
+  }
+
   return (
-    <header className={classes.header}>
-      <button className={classes.navBtn} onClick={responsiveNav}>
+    <header className={`${classes.header} ${responsiveHeader}`}>
+      <button
+        className={`${classes.navBtn} ${hidden}`}
+        onClick={responsiveHandler}
+      >
         <FaBars />
       </button>
-      <nav ref={navRef} className={classes["nav"]}>
+      <nav className={`${classes["nav"]} ${responsive}`}>
+        <button
+          className={`${classes.navBtn} ${classes.navBtnClose} `}
+          onClick={responsiveHandler}
+        >
+          <FaTimes />
+        </button>
         <a className={classes.linkNav} href="#contact-container">
-          Contact infos
+          Contact info
         </a>
         <a className={classes.linkNav} href="#about-tittle">
           About me
@@ -26,9 +49,6 @@ const Navbar = function Navbar(props) {
         <a className={classes.linkNav} href="main">
           Home
         </a>
-        <button className={classes.navBtn} onClick={responsiveNav}>
-          <FaTimes />
-        </button>
       </nav>
     </header>
   );
