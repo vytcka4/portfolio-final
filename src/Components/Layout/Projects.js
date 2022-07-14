f26daadc8e0b5206e952d8665e1a34f1fd5afe01
vytcka4 <vytautas.freelance@gmail.com>
2022-07-14 19:14:43 +0300

    commit

--- src/Components/Layout/Projects.js
+++ src/Components/Layout/Projects.js
@@ -2,6 +2,7 @@ import classes from "./Projects.module.css";
 import ProjectContainer from "../UI/ProjectContainer";
 import ProjectDescription from "../UI/ProjectDescription";
 import bankistWeb from "../store/bankistWeb.png";
+import mapty from "../store/mapty.png";
 
 function Projects() {
   return (
@@ -12,13 +13,17 @@ function Projects() {
           <img src={bankistWeb} alt="bankist" />
         </div>
         <ProjectDescription
+          descprition={"HTML5 ,CSS, javaSript"}
           name={"Bankist"}
           code={"https://github.com/vytcka4/bankist-website"}
         ></ProjectDescription>
       </ProjectContainer>
       <ProjectContainer>
-        <div className={classes.imgContainer}></div>
+        <div className={classes.imgContainer}>
+          <img src={mapty} alt="mapty" />
+        </div>
         <ProjectDescription
+          descprition={"HTML5 ,CSS, javaSript"}
           name={"Mapty"}
           code={"https://github.com/vytcka4/Mapty"}
         ></ProjectDescription>
